aaa6dad54457ecd711e5694a853987a3bd470e15
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2024-08-02 23:57:12 +0700

    Добавлены расширения для работы с узлами и ребрами

---
+++ VectorFEM.Core/Extensions/EdgeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using FEM.Common.Data.Domain;
+using VectorFEM.Core.Data.Parallelepipedal;
+
+namespace VectorFEM.Core.Extensions;
+
+public static class EdgeExtensions
+{
+    public static Task<int> ResolveLocal(this Edge edge, FiniteElement element)
+    {
+        var edges = element.Edges;
+        for (var i = 0; i < 12; i++)
+            if (edge.EdgeIndex == edges[i].EdgeIndex)
+                return Task.FromResult(i);
+
+        return Task.FromResult(0);
+    }
+
+    public static Task<int> StrataByEdges(this Edge edge, Mesh strata)
+    {
+        for (int i = 0; i < strata.Elements.Count; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                if (strata.Elements[i].Edges[j].EdgeIndex == edge.EdgeIndex)
+                {
+                    return Task.FromResult(i);
+                }
+            }
+        }
+
+        return Task.FromResult(0);
+    }
+}
