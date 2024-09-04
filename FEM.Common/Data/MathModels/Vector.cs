d6a6a9d3b621e2ddcaa18f8a5c4d373fd1ba94e4
Mihkail Barsukov <hardbasser322@gmail.com>
2024-09-04 23:01:59 +0700

    Добавил лослу. Навернул абстракций ты, конечно, и говна вместе с ними

--- FEM.Common/Data/MathModels/Vector.cs
+++ FEM.Common/Data/MathModels/Vector.cs
@@ -2,7 +2,19 @@
 
 public record Vector
 {
-    public IReadOnlyList<double> Data { get; init; } = [];
+    public List<double> Data { get; init; } = [];
+
+    public Vector() { }
+
+    public Vector(int size) => Data = [size];
+
+    public Vector(List<double> nums) => Data = nums;
+
+    public double this[int index]
+    {
+        get => Data[index];
+        set => Data[index] = value;
+    }
 
     public static Vector operator *(Vector vector, double unit) =>
         new() { Data = vector.Data.Select(item => item * unit).ToList() };
@@ -14,4 +26,9 @@ public record Vector
 
     public static double operator *(Vector vector1, Vector vector2) =>
         vector1.Data.Select((i, j) => i * vector2.Data[j]).Sum();
+
+    public static Vector operator *(double coef, Vector vector) => new(vector.Data.Select(num => num * coef).ToList());
+
+    public static Vector operator -(Vector vector1, Vector vector2) =>
+        new Vector { Data = vector1.Data.Select((i, j) => i - vector2.Data[j]).ToList() };
 }
---
+++ FEM.Solvers/Solvers/LosLU.cs
@@ -0,0 +1,128 @@
+﻿
+
+using FEM.Common.Data.MathModels;
+using FEM.Common.Data.MathModels.MatrixFormats;
+
+namespace SlaeSolver.Implementations.Solvers
+{
+    public class LosLU
+    {
+        private readonly int maxItCount;
+        private readonly double eps;
+
+        public LosLU(int maxItCount, double eps)
+        {
+            this.maxItCount = maxItCount;
+            this.eps = eps;
+        }
+
+        public Vector Solve(MatrixProfileFormat slae)
+        {
+            var vectors = ILUsqModi(slae);
+            var D = vectors.Item1;
+            var L = vectors.Item2;
+            var U = vectors.Item3;
+            return LOSLU(slae, D, L, U);
+        }
+
+
+        private Vector LOSLU(MatrixProfileFormat slae, Vector D, Vector L, Vector U)
+        {
+            var size = slae.Size;
+            double a, b;
+            Vector solve = new Vector(size);
+            Vector r, z, p, Ur, LAUr;
+
+            int k = 0;
+
+            r = ForwardStepModi(slae, slae.F, D, L);
+            z = BackStepModi(slae, r, U);
+            p = ForwardStepModi(slae, (slae * z).Data, D, L);
+
+            do
+            {
+                k++;
+
+                a = GetCoefficent(p, r, p, p);
+                solve += a * z;
+                r -= a * p;
+
+                Ur = BackStepModi(slae, r, U);
+                LAUr = ForwardStepModi(slae, (slae * Ur).Data, D, L);
+
+                b = -GetCoefficent(p, LAUr, p, p);
+
+                z = Ur + (b * z);
+
+                p = LAUr + (b * p);
+
+            } while (r * r > eps && k <= maxItCount);
+            Console.WriteLine($"Solver iterations count - {k}");
+            return solve;
+        }
+
+        private double GetCoefficent(Vector a1, Vector b1, Vector a2, Vector b2) => a1 * b1 / (a2 * b2);
+
+        private (Vector, Vector, Vector) ILUsqModi(MatrixProfileFormat matrix)
+        {
+            var D = new Vector(matrix.Di);
+            var L = new Vector(matrix.Gg);
+            var U = new Vector(matrix.Gg);
+
+            for (int i = 0; i < matrix.Size; i++)
+            {
+                double ls, us, d = 0;
+                int temp = matrix.Ig[i];
+                for (int j = matrix.Ig[i]; j < matrix.Ig[i + 1]; j++)
+                {
+                    ls = 0;
+                    us = 0;
+                    for (int h = matrix.Ig[matrix.Jg[j]], k = temp; h < matrix.Ig[matrix.Jg[j] + 1] && k < j;)
+                        if (matrix.Jg[k] == matrix.Jg[h])
+                        {
+                            ls += L[k] * U[h];
+                            us += L[h++] * U[k++];
+                        }
+                        else
+                        {
+                            if (matrix.Jg[k] < matrix.Jg[h])
+                                k++;
+                            else
+                                h++;
+                        }
+                    L[j] -= ls;
+                    U[j] = (U[j] - us) / D[matrix.Jg[j]];
+                    d += L[j] * U[j];
+                }
+                D[i] -= d;
+            }
+            return (D, L, U);
+        }
+
+        private Vector ForwardStepModi(MatrixProfileFormat matrix, List<double> rightSide, Vector D, Vector L)
+        {
+            Vector result = new Vector(matrix.Size);
+
+            for (int i = 0; i < matrix.Size; i++)
+            {
+                var sum = .0;
+                for (int j = matrix.Ig[i]; j < matrix.Ig[i + 1]; j++)
+                    sum += result[matrix.Jg[j]] * L[j];
+
+                result[i] = (rightSide[i] - sum) / D[i];
+            }
+            return result;
+        }
+
+        private Vector BackStepModi(MatrixProfileFormat matrix, Vector rightSide, Vector U)
+        {
+            Vector result = new Vector(rightSide.Data);
+
+            for (int i = matrix.Size - 1; i >= 0; i--)
+                for (int j = matrix.Ig[i]; j < matrix.Ig[i + 1]; j++)
+                    result[matrix.Jg[j]] -= result[i] * U[j];
+
+            return result;
+        }
+    }
+}
